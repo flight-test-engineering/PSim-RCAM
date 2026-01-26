101ac9f2aeba9f784db099cec435e96171a51035
Andre Celere <flight.test.engineering@gmail.com>
2026-01-26 07:36:17 -0500

    added console handler to logger

--- psim/helpers.py
+++ psim/helpers.py
@@ -5,6 +5,7 @@ import logging
 from datetime import date
 import os
 import queue
+import sys
 
 # create logger
 logger = logging.getLogger('psim_logger')
@@ -16,12 +17,18 @@ if not logger.handlers:
     file_handler = logging.FileHandler(f'{date.today()}_psim_log_file.log')
     file_handler.setLevel(logging.DEBUG)
 
+    # create a stream handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.DEBUG)
+
     # define log format
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
 
     # add file handler to logger
     logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
     
 
 def disk_logging_worker(log_queue, filename, header):
